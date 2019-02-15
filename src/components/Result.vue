a9ea8fe75e0deb1f528efde3689bfda48e97eb6b
vinci zhou <vinciarts@gmail.com>
2019-02-15 20:21:30 +0800

    update new steps

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
-.result
-  .logo.shadow-l logo 预览加水印
-  button 下载普通版 69元/次
-  button 下载专业版 129元/次
+.stack-y
+  .logo.shadow-l.radius-m.c-0 logo 预览加水印
+  button 下载普通版 69元/次(点击支付)
+  button 下载专业版 129元/次(点击支付)
   button 成为会员无限次下载
 </template>
 
@@ -17,5 +17,10 @@ export default {
 
 <style scoped lang="stylus">
 @require '~@/styles/layout/_index'
+
+.stack-y
+  stack(y, gap: var(--length-l))
+.logo
+  padding 1rem
 
 </style>
