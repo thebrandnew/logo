aff568a54e8cddeb9c70e7b7d6954c5b46d18085
vinci zhou <vinciarts@gmail.com>
2019-02-24 20:38:48 +0800

    Update Demo.vue

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,15 +1,49 @@
 <template lang="pug">
-.demo 这是个demo
+.demo
+  .logo-temp.shadow-l.radius-m.b-0
+    .x
+      .symbol
+      .group.y
+        .name 四季花海
+        .slogan Four Seasons
 </template>
 
 <script>
 export default {
   name: 'Demo',
   data () {
-    return {}
+    return {
+      layout: "x"
+    }
   }
 }
 </script>
 
 <style scoped lang="stylus">
+@require '~@/styles/layout/_index'
+.logo-temp
+  width 400px
+  height 300px
+  grid(x-arr:center, y-arr:center)
+
+.symbol
+  background red
+  width 80px
+  height 80px
+.name
+  font-size 24px
+  font-weight 800
+  line-height 100%
+.slogan
+  font-size 16px
+  font-weight 400
+  line-height 100%
+.x
+  stack(x, gap: 16px, ali: center)
+.y
+  stack(y, gap: 4px)
+
+
+
+
 </style>
